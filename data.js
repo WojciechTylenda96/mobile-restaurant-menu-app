17ad67aa2a9b434fc4038c9ee4a1d80dd6b0b51e
Wojciech <wojciech.tylenda.96@gmail.com>
2025-03-06 20:12:30 +0100

    add some menu items to data

---
+++ data.js
@@ -0,0 +1,24 @@
+
+const menuArray = [
+    {
+        name: 'Spaghetti bolognese',
+        ingredients: ['pasta', 'meat', 'tomatoes', 'basil', 'oregano', 'parmegiano'],
+        id: 0,
+        price: 15,
+        img: 'images/bolognese.png'
+    },
+    {
+        name: 'Spaghetti carbonara',
+        ingredients: ['pasta', 'meat', 'eggs', 'cheese', 'garlic'],
+        id: 1,
+        price: 13,
+        img: 'images/carbonara.jpg'
+    },
+    {
+        name: 'Tagliatelle with chicken',
+        ingredients: ['pasta', 'chicken breasts', 'onion', 'cream', 'olive oil'],
+        id: 2,
+        price: 14,
+        img: 'images/tagliatelle.jpg'
+    },
+]
